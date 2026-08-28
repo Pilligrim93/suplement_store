f4db81b7e51a0dda1c7b164807bb6e883a433619
Neo <neo76941@gmail.com>
2026-08-28 06:30:54 +0300

    Внедрен авто-прогрев кеша через Entrypoint
    Создана Django-команда warmup_catalog
    
    Написан bash-скрипт entrypoint.sh для обхода пулера при миграциях
    
    Обновлены Dockerfile и docker-compose.yml

--- goods/management/commands/warmup_catalog.py
+++ goods/management/commands/warmup_catalog.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
 
-    help = "Высоконагруженный разовый прогрев кэша всего каталога товаров в ОП (Инстанс 4)"
+    help = "Высоконагруженный разовый прогрев кэша всего каталога товаров в (Инстанс 4)"
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.WARNING("[Warmup] Инициализация прогрева каталога..."))
@@ -11,8 +11,7 @@ class Command(BaseCommand):
             # Скрытый импорт таски внутри метода handle для защиты от Circular Imports!
             from goods.tasks import task_write_all_catalog_in_cache
 
-            # Отправляем задачу в 3-й инстанс брокера (порт 6381)
-            # .delay() срабатывает за микросекунду, Django не ждет завершения и идет дальше
+            # Отправляю задачу в 3-й инстанс брокера (порт 6381)
             task_write_all_catalog_in_cache.delay()
 
             self.stdout.write(
